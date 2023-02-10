330d2e170ec294dd32e7b0f08fa9a4a250ea508d
Admin <Admin@TEB12707>
2023-02-10 10:12:16 +0100

    Add project files.

---
+++ Inlämningsuppgift1metoder/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace metoduppgift1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Tal(5,76,12));
+        }
+        static int Tal(int tal1,int tal2,int tal3)
+        {
+            return tal1 + tal2 + tal3;
+        }
+    }
+}
